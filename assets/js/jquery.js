8d60316eed711785ac6b57487786b507926af71b
Sujan Saravanamuthu <sujansaravanamuthu@gmail.com>
2021-02-20 17:06:04 -0500

    modal

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
       $(".prev span").text(y);
     });
     
-    $("#pop").on("click", function() {
+    $(".pop").on("click", function() {
       $('#imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
       $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
    });
