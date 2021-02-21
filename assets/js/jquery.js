2112fe39f24592b992998b6f901e299b3c55e232
Sujan Saravanamuthu <sujansaravanamuthu@gmail.com>
2021-02-20 21:44:57 -0500

    change

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,15 +1,15 @@
-$(document).ready(function () {
-  
-  $(".nav-tabs a").click(function () {
-    $(this).tab('show');
+$(document).ready(function(){
+    $(".nav-tabs a").click(function(){
+      $(this).tab('show');
+    });
+    $('.nav-tabs a').on('shown.bs.tab', function(event){
+      var x = $(event.target).text();         // active tab
+      var y = $(event.relatedTarget).text();  // previous tab
+      $(".act span").text(x);
+      $(".prev span").text(y);
+    });  
+    setTimeout(function() {
+      ('#research').trigger("click");
+      console.log('hello');
+     },3000);
   });
-  
-  $('.nav-tabs a').on('shown.bs.tab', function (event) {
-    var x = $(event.target).text(); // active tab
-    var y = $(event.relatedTarget).text(); // previous tab
-    $(".act span").text(x);
-    $(".prev span").text(y);
-  });
-  
-  $('#research').trigger('click');
-});
